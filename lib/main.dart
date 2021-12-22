89e6987e72c42f9fed06e5fa12676f9d3fe3d0bb
خلود عبد الله باتيس <50932099+KholoudBatis@users.noreply.github.com>
2021-12-22 08:16:35 +0300

    ad state less widget
    this add is important to be run quickly

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.yellow[50],
         appBar: AppBar(
@@ -15,6 +24,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
